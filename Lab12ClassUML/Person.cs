51b07af207ae809e69e3162596729d880376231d
[Nate] <[ncggardner@gmail.com]>
2020-05-05 19:11:05 -0400

    first commit

---
+++ Lab12ClassUML/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab12ClassUML
+{
+    class Person
+    {
+        private string Name { get; set; }
+        private string Address { get; set; }
+
+
+        public virtual string ToString()
+        {
+            return ($"{Name} + {Address}");
+        }
+    }
+}
